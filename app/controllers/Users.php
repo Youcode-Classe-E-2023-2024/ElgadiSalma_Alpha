81e04560a52882653d20add1eb5c089f0d980e4e
Elgadisalma <elgadi.salma0@gmail.com>
2024-01-03 20:21:37 +0100

    Update Users.php

--- app/controllers/Users.php
+++ app/controllers/Users.php
@@ -2,11 +2,11 @@
 
 Class Users extends Controller
 {
-    private $usersModel;
+    private $userModel;
 
     public function __construct()
     {
-        $this->usersModel = $this->model('users');
+        $this->userModel = $this->model('User');
     }
     
     public function login()
@@ -15,18 +15,73 @@ Class Users extends Controller
         {
             $email = $_POST['email'];
             $password = $_POST['password'];
+            $email_err = '';
+            $password_err = '';
 
-            $loggedInUser = $this->usersModel->login($email, $password);
-            if ($loggedInUser) 
+      
+            // Validate Email
+            if (empty($email)) 
             {
-                echo'wee';  
-            } else 
-            {
-            echo'laaa';
+                $email_err = 'Please enter email';
             }
-        }     
+      
+            // Validate Password
+            if (empty($password)) 
+            {
+                $password_err = 'Please enter password';
+            }
+
+            if (empty($email_err) && empty($password_err)) 
+            {
+                $loggedInUser = $this->userModel->login($email, $password);
+                if ($loggedInUser) 
+                    {
+                        echo'wee';  
+                    } else 
+                    {
+                    $password_err = 'Password not correct';
+                    }
+                    } 
+                else 
+                {
+                // User not found
+                $email_err = 'No user found';
+                }
+
+            // $this->view('users/login', compact('email_err', 'password_err'));
+        }
+        else{
+
+            return $this->view('users/login');
+         }
         
     }
 
 
-}
+    public function addUsers()
+    {
+        if ($_SERVER['REQUEST_METHOD'] == 'POST') {    
+            $postData = file_get_contents("php://input");
+            $data = json_decode($postData);
+            // echo(json_encode($data->username));die;
+            $username = $data->username;
+            $email = $data->email;
+            $password = $data->password;
+
+            if (!empty($username && $email && $password)) {
+                $add = $this->userModel->addUsers($username, $email, $password);
+                echo json_encode($add);die;
+                if ($add) {
+                    echo json_encode(array('message' => true));
+                } else {
+                    echo json_encode(array('message' => false));
+                }
+            } else {
+                echo json_encode(['message' => 'invalid request']);
+            }
+        } else {
+            return $this->view('users/add');
+        }
+    }       
+
+}   
