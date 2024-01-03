76e90ecc42621da7b69d684fbd62041793135368
Elgadisalma <elgadi.salma0@gmail.com>
2024-01-03 01:58:44 +0100

    Update config.php

--- app/config/config.php
+++ app/config/config.php
@@ -3,7 +3,7 @@
   define("DB_HOST", "localhost");
   define("DB_USER", "root");
   define("DB_PASS", "");
-  define("DB_NAME", "suivie_hachami");
+  define("DB_NAME", "alpha");
 
   // App Root
   define('APPROOT', dirname(dirname(__FILE__)));
