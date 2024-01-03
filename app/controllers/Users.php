a3ac6283d23f54659caf8cda7ef9abb36c7f74eb
Elgadisalma <elgadi.salma0@gmail.com>
2024-01-03 02:29:12 +0100

    Update Users.php

--- app/controllers/Users.php
+++ app/controllers/Users.php
@@ -15,41 +15,16 @@ Class Users extends Controller
         {
             $email = $_POST['email'];
             $password = $_POST['password'];
-            $email_err = '';
-            $password_err = '';
 
-      
-            // Validate Email
-            if (empty($email)) 
+            $loggedInUser = $this->usersModel->login($email, $password);
+            if ($loggedInUser) 
             {
-                $email_err = 'Please enter email';
+                echo'wee';  
+            } else 
+            {
+            echo'laaa';
             }
-      
-            // Validate Password
-            if (empty($password)) 
-            {
-                $password_err = 'Please enter password';
-            }
-
-            if (empty($email_err) && empty($password_err)) 
-            {
-                $loggedInUser = $this->usersModel->login($email, $password);
-                if ($loggedInUser) 
-                    {
-                        echo'wee';  
-                    } else 
-                    {
-                    $password_err = 'Password not correct';
-                    }
-                    } 
-                else 
-                {
-                // User not found
-                $email_err = 'No user found';
-                }
-
-            $this->view('users/login', compact('email_err', 'password_err'));
-        }
+        }     
         
     }
 
