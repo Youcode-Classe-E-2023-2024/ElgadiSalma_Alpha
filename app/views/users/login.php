c15da3a6d7676487e1f031613eb8435db93fc439
Elgadisalma <elgadi.salma0@gmail.com>
2024-01-04 10:20:47 +0100

    Update login.php

--- app/views/users/login.php
+++ app/views/users/login.php
@@ -12,7 +12,7 @@
         <svg class="absolute ml-3" width="24" viewBox="0 0 24 24">
           <path d="M20.822 18.096c-3.439-.794-6.64-1.49-5.09-4.418 4.72-8.912 1.251-13.678-3.732-13.678-5.082 0-8.464 4.949-3.732 13.678 1.597 2.945-1.725 3.641-5.09 4.418-3.073.71-3.188 2.236-3.178 4.904l.004 1h23.99l.004-.969c.012-2.688-.092-4.222-3.176-4.935z"/>
         </svg>
-        <input type="text" name="email" class="bg-gray-200 pl-12 py-2 md:py-4 focus:outline-none w-full" placeholder="Username" />
+        <input type="email" name="email" class="bg-gray-200 pl-12 py-2 md:py-4 focus:outline-none w-full" placeholder="Email" />
         <span class="invalid-feedback"><?php if(!empty($email_err)) { echo $email_err; } ?></span>
 
       </div>
@@ -26,5 +26,9 @@
       </div>
       <button type="submit" class="bg-gradient-to-b from-gray-700 to-gray-900 font-medium p-2 md:p-4 text-white uppercase w-full">Login</button>
     </form>
+    <a href="<?php echo URLROOT; ?>/users/register">
+    <button class="bg-gradient-to-b from-gray-700 to-gray-900 font-medium p-2 md:p-4 text-white uppercase w-full">Register</button>
+    </a>
   </div>
  </div>
+ <?php require APPROOT . '/views/inc/footer.php'; ?>
