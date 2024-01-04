81ec9387e8e123dd12dc215b77ff958ca05b5f5b
Elgadisalma <elgadi.salma0@gmail.com>
2024-01-04 17:31:49 +0100

    Update main.js

--- public/js/main.js
+++ public/js/main.js
@@ -1,88 +1,125 @@
-console.log("saaa");
 let endpoint = 'http://localhost/ElgadiSalma_Alpha/Users/';
 const addContainer = document.getElementById('addContainer');
 const addButton = document.getElementById('addButton');
 const saveButton = document.getElementById('saveButton'); // Ajout du bouton "Save"
+const userContainer = document.getElementById('usersContainer');
+
 
 let formIndex = 1;
 let userArray = []; // Tableau pour stocker les données des utilisateurs
 
-addButton.addEventListener('click', function (event) {
-    addContainer.insertAdjacentHTML('beforeend', createUserFormHTML(formIndex));
-    formIndex++;
-});
+    addButton.addEventListener('click', function (event) {
+        addContainer.insertAdjacentHTML('beforeend', createUserFormHTML(formIndex));
+        formIndex++;
+    });
 
-saveButton.addEventListener('click', function (event) {
-    addUser();
-    saveUsers(); 
-});
+    saveButton.addEventListener('click', function (event) {
+        addUser();
+        saveUsers(); 
+    });
 
-function addUser() {
-    const usernames = document.querySelectorAll('[name^="username"]');
-    const emails = document.querySelectorAll('[name^="email"]');
-    const passwords = document.querySelectorAll('[name^="password"]');
-    
-    const userData = {
-        usernames: Array.from(usernames).map(input => input.value.trim()),
-        emails: Array.from(emails).map(input => input.value.trim()),
-        passwords: Array.from(passwords).map(input => input.value.trim()),
-    };
-
-    if (userData.usernames.every(username => username !== '') && 
-        userData.emails.every(email => email !== '') && 
-        userData.passwords.every(password => password !== '')) {
-
-        userArray.push(userData);
+    function addUser() 
+    {
+        const usernames = document.querySelectorAll('[name^="username"]');
+        const emails = document.querySelectorAll('[name^="email"]');
+        const passwords = document.querySelectorAll('[name^="password"]');
         
-        usernames.forEach(username => username.value = '');
-        emails.forEach(email => email.value = '');
-        passwords.forEach(password => password.value = '');
+        const userData = {
+            usernames: Array.from(usernames).map(input => input.value.trim()),
+            emails: Array.from(emails).map(input => input.value.trim()),
+            passwords: Array.from(passwords).map(input => input.value.trim()),
+        };
 
-        console.log('User Array:', userArray);
+        if (userData.usernames.every(username => username !== '') && 
+            userData.emails.every(email => email !== '') && 
+            userData.passwords.every(password => password !== '')) {
+
+            userArray.push(userData);
+            
+            usernames.forEach(username => username.value = '');
+            emails.forEach(email => email.value = '');
+            passwords.forEach(password => password.value = '');
+
+            console.log('User Array:', userArray);
+        }
     }
-}
 
-function saveUsers() {
-    if (userArray.length > 0) {
-        fetch(endpoint + 'addUsers', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(userArray), // Envoyer tous les utilisateurs
-        })
+    function saveUsers() 
+    {
+        if (userArray.length > 0) 
+        {
+            fetch(endpoint + 'addUsers', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(userArray), // Envoyer tous les utilisateurs
+            })
+            .then(response => response.json())
+            .then(data => { 
+                console.log(data);
+                userArray = []; // Réinitialisation du tableau après sauvegarde
+            })
+            .catch(error => {
+                console.error('Erreur', error);
+            });
+        }
+    }
+
+    function createUserFormHTML(index) 
+    {
+        return `
+            <br>
+            <h2 class="text-center drop-shadow-md font-bold text-2xl uppercase mb-10">Post </h2>
+            <div>
+                <div class="mb-5">
+                    <label for="username" class="block mb-2 font-bold text-gray-600 uppercase">username</label>
+                    <input type="text" id="username" name="username${index}" placeholder="UserName." class="border border-gray-300 shadow p-3 w-full rounded ">
+                </div>
+
+                <div class="mb-5">
+                    <label for="email" class="block mb-2 font-bold text-gray-600 uppercase">Email</label>
+                    <input type="email" id="email" name="email${index}" placeholder="Email." class="border border-gray-300 shadow p-3 w-full rounded ">
+                </div>
+
+                <div class="mb-5">
+                    <label for="password" class="block mb-2 font-bold text-gray-600 uppercase">password</label>
+                    <input type="password" id="password" name="password${index}" placeholder="password." class="border border-gray-300 shadow p-3 w-full rounded ">
+                </div>
+            </div>
+            <div id="dynamicDivContainer"></div>
+            <div class="flex justify-between">
+        `;
+    }
+
+    fetch(endpoint+`displayAll`)
         .then(response => response.json())
-        .then(data => { 
-            console.log(data);
-            userArray = []; // Réinitialisation du tableau après sauvegarde
+        .then(data => {
+        // publication = data;
+
+        displayData(data);
         })
         .catch(error => {
-            console.error('Erreur', error);
+        console.error('Error:', error);
         });
+
+
+    function displayData(data)
+    {
+        const rows = data.map((user) => {
+            
+            return (
+        `
+        <td class="p-3 px-5">${user.username}</td>
+        <td class="p-3 px-5">${user.email}</td>
+        <td class="p-3 px-5 flex gap-5 justify-end">
+            <button name="supprimer" class="text-sm bg-red-500 hover:bg-red-700 text-white py-1 px-2 rounded focus:outline-none focus:shadow-outline">Supprimer</button>
+            <button name="blocker" class="text-sm bg-red-500 hover:bg-red-700 text-white py-1 px-2 rounded focus:outline-none focus:shadow-outline">blocker</button>
+        </td>
+
+        `
+            );
+        });
+        console.log(rows);
+        userContainer.innerHTML = rows;
     }
-}
-
-function createUserFormHTML(index) {
-    return `
-        <br>
-        <h2 class="text-center drop-shadow-md font-bold text-2xl uppercase mb-10">Post </h2>
-        <div>
-            <div class="mb-5">
-                <label for="username" class="block mb-2 font-bold text-gray-600 uppercase">username</label>
-                <input type="text" id="username" name="username${index}" placeholder="UserName." class="border border-gray-300 shadow p-3 w-full rounded ">
-            </div>
-
-            <div class="mb-5">
-                <label for="email" class="block mb-2 font-bold text-gray-600 uppercase">Email</label>
-                <input type="email" id="email" name="email${index}" placeholder="Email." class="border border-gray-300 shadow p-3 w-full rounded ">
-            </div>
-
-            <div class="mb-5">
-                <label for="password" class="block mb-2 font-bold text-gray-600 uppercase">password</label>
-                <input type="password" id="password" name="password${index}" placeholder="password." class="border border-gray-300 shadow p-3 w-full rounded ">
-            </div>
-        </div>
-        <div id="dynamicDivContainer"></div>
-        <div class="flex justify-between">
-    `;
-}
