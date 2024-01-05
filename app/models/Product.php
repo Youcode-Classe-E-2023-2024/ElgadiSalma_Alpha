b6c2fab43122343ce026c0348ba72d38ea89dc96
Elgadisalma <elgadi.salma0@gmail.com>
2024-01-05 22:55:01 +0100

    Update Product.php

--- app/models/Product.php
+++ app/models/Product.php
@@ -7,19 +7,47 @@
       $this->db = new Database;
     }
 
-    public function allPublications()
+    public function allProducts()
     {
-        $this->db->query("SELECT * FROM publication ");
-        return $this->db->resultset();
+      $this->db->query("SELECT * FROM product");
+      $products = $this->db->resultset();
+      return $products;
     }
 
-    public function addPub($title, $price, $createdBy)
+    public function addProduct($title, $description)
     {
-        $this->db->query("INSERT INTO publication (title, price, created_by) VALUES('$title', '$price', '$createdBy')");
-        if($this->db->execute()){
+        $this->db->query('INSERT INTO product (title, description) VALUES(:title, :description)');
+        
+        $this->db->bind(':title', $title);
+        $this->db->bind(':description', $description);
+        
+        return $this->db->execute();
+    }
+
+    public function deleteProduct($id)
+    {
+      $this->db->query("DELETE FROM Product WHERE id_product = :id");
+      $this->db->bind(':id', $id);
+      if($this->db->execute())
+      {
+      return true;
+      } else {
+          return false;
+      }
+    }
+
+    public function editProduct($idProduct, $title, $description)
+    {
+        $this->db->query('UPDATE product SET title = :title, description = :description WHERE id_product = :idProduct');
+
+        $this->db->bind(':title', $title);
+        $this->db->bind(':description', $description);
+        $this->db->bind(':idProduct', $idProduct);
+
+        if ($this->db->execute()) {
             return true;
         } else {
             return false;
-          }
+        }
     }
   }
