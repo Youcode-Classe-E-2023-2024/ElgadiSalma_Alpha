ba75baccedf8357d17e9d9e01945579483d57e03
Elgadisalma <elgadi.salma0@gmail.com>
2024-01-06 22:52:05 +0100

    Update graphe.js

--- public/js/graphe.js
+++ public/js/graphe.js
@@ -1,14 +1,14 @@
-// Ajoutez ce code dans votre fichier graphe.js
+// User Graphe
 fetch('http://localhost/ElgadiSalma_Alpha/Pages/grapheUser')
     .then(response => response.json())
     .then(data => {
-        createChart(data);
+        createUserChart(data); // Appelle la fonction spécifique pour les utilisateurs
     })
     .catch(error => {
         console.error('Error:', error);
     });
 
-function createChart(data) {
+function createUserChart(data) {
     const dates = data.map(entry => entry.date);
     const counts = data.map(entry => entry.user_count);
 
@@ -44,3 +44,54 @@ function createChart(data) {
         }
     });
 }
+
+// End User Graphe
+
+// Product Graphe
+fetch('http://localhost/ElgadiSalma_Alpha/Pages/grapheProduct')
+    .then(response => response.json())
+    .then(data => {
+        createProductChart(data); // Appelle la fonction spécifique pour les produits
+    })
+    .catch(error => {
+        console.error('Error:', error);
+    });
+
+function createProductChart(data) {
+    const dates = data.map(entry => entry.date);
+    const counts = data.map(entry => entry.product_count);
+
+    const ctx = document.getElementById('productChart').getContext('2d');
+    const productChart = new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: dates,
+            datasets: [{
+                label: 'Nombre de produits ajoutés',
+                data: counts,
+                backgroundColor: 'rgba(75, 192, 192, 0.2)',
+                borderColor: 'rgba(75, 192, 192, 1)',
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                x: {
+                    title: {
+                        display: true,
+                        text: 'Date'
+                    }
+                },
+                y: {
+                    beginAtZero: true,
+                    title: {
+                        display: true,
+                        text: 'Nombre de produits'
+                    }
+                }
+            }
+        }
+    });
+}
+
+// End Product Graphe
