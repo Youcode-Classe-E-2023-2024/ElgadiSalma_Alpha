3653b0d42677007f4f5ba3fa1a7573a6e59565c5
Elgadisalma <elgadi.salma0@gmail.com>
2024-01-08 06:28:39 +0100

    Create notification.php

---
+++ app/views/pages/notification.php
@@ -0,0 +1,20 @@
+<?php require APPROOT . '/views/inc/header.php'; ?>
+<?php require APPROOT . '/views/inc/navbar.php'; ?>
+<div class="bg-gray-100 flex flex-col  w-full">
+
+   <div class="px-24 min-h-screen place-items-center bg-slate-900">
+   <h1 class="text-4xl text-purple-400 text-center py-24 font-bold">Notifications</h1>
+
+  <div class="w-full max-w-3xl rounded-lg bg-gradient-to-br from-fuchsia-600 to-indigo-500 ">
+    <div class="relative m-px overflow-hidden rounded-lg bg-slate-900 px-3 py-3 text-lg">
+      <div class="absolute -top-[50px] -left-[15px] z-0 h-[140px] w-[140px] rounded-full border bg-pink-500 opacity-20 blur-[100px]"></div>
+      <div class="relative z-10 text-center">
+        <p class="text-sm "> Get 30% off on . Valid until 28th Nov. Use code: <strong class="">salma</strong> </p>
+      </div>
+    </div>
+  </div>
+</div> 
+</div>
+
+
+<?php require APPROOT . '/views/inc/footer.php'; ?>
